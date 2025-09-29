81f859e28c492325be9efd62c7a108ca0b3d3598
kartik-rao-p <kartikraopatange@gmail.com>
2025-09-29 10:22:06 +0530

    Update color_code_test.h

--- color_code_test.h
+++ color_code_test.h
@@ -1,7 +1,7 @@
 #ifndef COLOR_CODE_TEST_H
 #define COLOR_CODE_TEST_H
 
-#include "color_code_definitions.h" // Include the definitions
+#include "color_code_definitions.hpp" // Include the definitions
 
 // Function declarations for testing
 void testNumberToPair(int pairNumber,
